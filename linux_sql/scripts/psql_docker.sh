3f8aeaf9aef7e2614a947e01e3682cd103f21124
natolcese2203 <139156276+natolcese2203@users.noreply.github.com>
2023-07-22 11:53:27 -0400

    Update psql_docker.sh

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -3,7 +3,7 @@
 # Run docker if it is not already running
 systemctl status docker || systemctl start docker
 
-# Get command line arguments
+
 command=$1
 db_username=$2
 db_password=$3
@@ -11,21 +11,20 @@ db_password=$3
 case $command in
 
   create)
-    # Check if jrvs-psql already exist
+    # Check if jrvs-psql already exists in system
     if [[ $(docker container ls -a -f name=jrvs-psql | wc -l) -eq 2 ]]; then
       echo "Warning: jrvs-psql container is already created"
       echo "USAGE: ./psql_docker.sh start|stop|create [db_username][db_password]"
       exit 0
     fi
 
-    # Check if username or password is not set
+  
     if [[ -z "$db_username" || -z "$db_password" ]]; then
       echo "Error: Username or password is not specified"
       echo echo "USAGE: ./psql_docker.sh start|stop|create [db_username][db_password]"
       exit 1
     fi
 
-    # Create a pgdata volume and jrvs-psql container
     docker volume create pgdata
     docker run --name jrvs-psql -e POSTGRES_USER="$db_username" -e POSTGRES_PASSWORD="$db_password" \
            -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres
@@ -41,7 +40,7 @@ case $command in
       exit 1
     fi
 
-    # Start or stop the jrvs-psql container
+  
     if [ "$command" == "start" ]; then
       docker container start jrvs-psql
     else
@@ -52,7 +51,7 @@ case $command in
     ;;
 
   *)
-    echo "Error: valid arguments not given"
+    echo "Error: valid arguments were not provides"
     echo "USAGE: ./psql_docker.sh start|stop|create [db_username][db_password]"
     exit 1
     ;;
