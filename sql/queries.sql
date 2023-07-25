4e8d79e3861c4e9eeb28b8571a0e0f90e7118e3c
natolcese2203 <139156276+natolcese2203@users.noreply.github.com>
2023-07-25 14:59:38 -0400

    Update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -126,7 +126,15 @@ select facid, extract(month from starttime) as month, sum(slots) as "Total Slots
 order by facid, month;      
 
 select count(distinct memid) from cd.bookings  
-
+	
+select mems.surname, mems.firstname, mems.memid, min(bks.starttime) as starttime
+	from cd.bookings bks
+	inner join cd.members mems on
+		mems.memid = bks.memid
+	where starttime >= '2012-09-01'
+	group by mems.surname, mems.firstname, mems.memid
+order by mems.memid;
+	
 	from cd.bookings bks
 	inner join cd.members mems on
 		mems.memid = bks.memid
