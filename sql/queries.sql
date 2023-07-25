4db6af0984cd6e479478c44e6b2501c18633bc4e
natolcese2203 <139156276+natolcese2203@users.noreply.github.com>
2023-07-25 13:08:34 -0400

    Update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,1 +1,50 @@
+insert into cd.facilities
+    (facid, name, membercost, guestcost, initialoutlay, monthlymaintenance)
+    values (9, 'Spa', 20, 30, 100000, 800);
 
+nsert into cd.facilities
+    (facid, name, membercost, guestcost, initialoutlay, monthlymaintenance)
+    select (select max(facid) from cd.facilities)+1, 'Spa', 20, 30, 100000, 800;  
+
+update cd.facilities facs
+    set
+        membercost = (select membercost * 1.1 from cd.facilities where facid = 0),
+        guestcost = (select guestcost * 1.1 from cd.facilities where facid = 0)
+    where facs.facid = 1;  
+
+update cd.facilities facs
+    set
+        membercost = (select membercost * 1.1 from cd.facilities where facid = 0),
+        guestcost = (select guestcost * 1.1 from cd.facilities where facid = 0)
+    where facs.facid = 1;          
+
+truncate cd.bookings;
+
+delete from cd.members where memid = 37;   
+
+
+select facid, name, membercost, monthlymaintenance 
+	from cd.facilities 
+	where 
+		membercost > 0 and 
+		(membercost < monthlymaintenance/50.0);          
+
+select *
+	from cd.facilities 
+	where 
+		name like '%Tennis%';    
+
+select *
+	from cd.facilities 
+	where 
+		facid in (1,5);  
+
+select memid, surname, firstname, joindate 
+	from cd.members
+	where joindate >= '2012-09-01';   
+
+select surname 
+	from cd.members
+union
+select name
+	from cd.facilities; 
