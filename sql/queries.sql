f835067721cb3cdddc860d4fb21d18f8c086c2b2
natolcese2203 <139156276+natolcese2203@users.noreply.github.com>
2023-07-25 15:04:33 -0400

    Update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -135,12 +135,6 @@ select mems.surname, mems.firstname, mems.memid, min(bks.starttime) as starttime
 	group by mems.surname, mems.firstname, mems.memid
 order by mems.memid;
 	
-	from cd.bookings bks
-	inner join cd.members mems on
-		mems.memid = bks.memid
-	where starttime >= '2012-09-01'
-	group by mems.surname, mems.firstname, mems.memid
-order by mems.memid; 
 
 select count(*) over(), firstname, surname
 	from cd.members
